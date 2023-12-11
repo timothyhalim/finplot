44b7c2276f5718bf16e248ebeaca7fdbdd7bdf4f
Timothy Halim <timothy.halim@live.com>
2023-12-11 22:40:31 +1100

    Change PyQt6 to PySide6

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/highfestiva/finplot',
     packages=['finplot'],
-    install_requires=['numpy>=1.23.5', 'pandas>=1.5.2', 'PyQt6>=6.4.0', 'pyqtgraph>=0.13.1', 'python-dateutil'],
+    install_requires=['numpy>=1.23.5', 'pandas>=1.5.2', 'PySide6>=6.4.0', 'pyqtgraph>=0.13.1', 'python-dateutil'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
